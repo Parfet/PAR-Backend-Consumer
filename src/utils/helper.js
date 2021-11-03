e737e54a6c1b277d67c890e628f78ffa7624c423
smurf <apiwat.aee@chakree.me>
2021-11-03 23:28:56 +0700

    🏔 [ADD] helper for get url from string

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -7,4 +7,8 @@ module.exports = {
   getKeyByValue: (object, value) => {
     return Object.keys(object).find((key) => object[key] === value);
   },
+  urlify: (text) => {
+    var urlRegex = /(((https?:\/\/)|(www\.))[^\s]+)/g;
+    return text.match(urlRegex);
+},
 };
